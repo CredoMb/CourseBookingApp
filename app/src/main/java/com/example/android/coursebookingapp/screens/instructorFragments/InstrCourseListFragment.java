644dd72df9506f6fac52eefdde7b4b4fee3610f0
Credo-Mbikayi <credo.mbikayi@umontreal.ca>
2021-07-01 19:15:41 -0400

    Start the instruction navigation

--- app/src/main/java/com/example/android/coursebookingapp/screens/instructorFragments/InstrCourseListFragment.java
+++ app/src/main/java/com/example/android/coursebookingapp/screens/instructorFragments/InstrCourseListFragment.java
@@ -1,4 +1,34 @@
 package com.example.android.coursebookingapp.screens.instructorFragments;
 
-public class InstrCourseListFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.android.coursebookingapp.database.CourseBookingDataBase;
+import com.example.android.coursebookingapp.database.CourseDAO;
+
+import java.util.ArrayList;
+
+public class InstrCourseListFragment extends Fragment {
+
+    private CourseDAO courseDAO;
+    //
+    private String courseName_;
+    private String courseCode_;
+
+    private ArrayList<String> courseArrList_;
+    private CourseBookingDataBase db;
+    private ArrayAdapter<String> adapter;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //return inflater;
+    }
 }
