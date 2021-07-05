fbbe1d6f879557b11774a343bd4848fec6b7f2ed
LordAndy316 <LordAndy316@gmail.com>
2021-07-05 01:42:48 -0400

    Andrew

---
+++ app/src/main/java/com/example/android/coursebookingapp/app/src/main/java/com/example/android/coursebookingapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.android.coursebookingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // Create an admin if he doesn't exist already
+    //
+}
